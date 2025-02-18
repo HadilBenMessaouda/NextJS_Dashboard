31b9b9c5dabb0ca1a1f41fcbfac5c01bb87f718c
Hadil Ben Messaouda <hadilbenmessaouda11@gmail.com>
2025-02-18 10:36:02 +0100

    dashboard with links

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    return config;
+  },
+  experimental: {
+    turbo: false,
+  },
 };
 
 export default nextConfig;
